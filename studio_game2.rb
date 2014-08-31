885f975ac500c298f0eb9311ebcb86eb117c4157
Sergio Barrera <sdbeng@gmail.com>
2014-08-31 11:40:38 -0700

    Calling other methods with studio2

--- studio_game2.rb
+++ studio_game2.rb
@@ -2,8 +2,24 @@
 #  health = health_val
 # end
 
-def say_hello(name)
- puts "I'm #{name.capitalize}."
+def say_hello(name, health=100)
+ "I'm #{name.capitalize} with a health of #{health}."
 end
 
-say_hello("larry")
+#puts say_hello("larry", 60)
+
+=begin
+The say_hello method takes a name parameter. 
+What do you suppose will happen if you try to run the following line
+ of code outside of the say_hello method?
+puts name
+
+Why do you get that error?
+Because name is a local variable inside the say_hello method and
+not accessible outside the method. In other words, name is not in scope.
+=end
+
+#Now, Set a Default Parameter Value for health if isn't passed in.
+puts say_hello("larry")
+
+#Calling Other Methods
